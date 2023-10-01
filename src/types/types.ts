df3eba5e4cc5aeea387b4fc2c24d0e63f0fdf6fd
temple69 <utemple169@gmail.com>
2023-10-01 01:01:05 +0100

    types used for the project

---
+++ src/types/types.ts
@@ -0,0 +1,19 @@
+// Types for dark mode
+export type darkModeType = {
+  isDarkMode: boolean;
+};
+//Types for github user
+export type githubUser = {
+  name: string;
+  username: string;
+  company: string;
+  location: string;
+  bio: string;
+  twitter_username: string;
+  public_repos: number;
+  followers: number;
+  following: number;
+  profile_url: string;
+  avatar_url: string;
+  created_at: string;
+};
